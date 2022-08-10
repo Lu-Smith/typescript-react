acf1d6d113c7aee6f7ef9daa7d56bb102faec722
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2022-08-10 19:30:54 +0100

    Added List.tsx
    Updated App.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import List from './components/List';
+
+interface IState {
+  people: {
+    name: string
+    age: number
+    url: string
+    note?: string
+  }[]
+}
 
 function App() {
 
+  const [people, setPeople] = useState<IState["people"]>([
+    {
+      name: "Luna Smith",
+      age: 37,
+      url: "https://images.pexels.com/photos/2422913/pexels-photo-2422913.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      note: "Life is good",
+    }
+  ]);
 
   return (
     <div className="App">
-      <h1>Hello World</h1>
-
+      <h1>Members</h1>
+      <List people={people}/>
     </div>
   );
 }
