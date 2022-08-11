24f45d374abfd33f1c45e8433a404740fff14acc
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2022-08-11 11:10:11 +0100

    Fixed the bug

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
 import React, { useState } from 'react';
 import './App.css';
-import AddToList from './components/AddToList';
 import List from './components/List';
+import AddToList from './components/AddToList';
 
 export interface IState {
   people: {
-    name: string
-    age: number
-    url: string
-    note?: string
+      name: string
+      age: number
+      img: string
+      note?: string
   }[]
 }
+
 
 function App() {
 
@@ -18,16 +19,22 @@ function App() {
     {
       name: "Luna Smith",
       age: 37,
-      url: "https://images.pexels.com/photos/2422913/pexels-photo-2422913.jpeg?auto=compress&cs=tinysrgb&w=1600",
-      note: "Life is good",
+      img: "https://images.pexels.com/photos/2397653/pexels-photo-2397653.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      note: "“Art is not a thing, it is a way.”",
+    },
+    {
+      name: "Ross Miller",
+      age: 35,
+      img: "https://images.pexels.com/photos/1851164/pexels-photo-1851164.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      note: "“The principle of true art is not to portray, but to evoke.”",
     }
-  ]);
+  ])
 
   return (
     <div className="App">
-      <h1>Members</h1>
+      <h1>Club Members</h1>
       <List people={people}/>
-      <AddToList people={people} setPeople={setPeople}/>
+      <AddToList setPeople={setPeople} people={people}/>
     </div>
   );
 }
--- src/components/AddToList.tsx
+++ src/components/AddToList.tsx
@@ -1,85 +1,89 @@
-
-import React, { useState } from 'react';
-import {IState as Props} from "../App";
+import React, { useState } from 'react'
+import { IState as Props } from "../App";
 
 interface IProps {
-   people: Props["people"]
-   setPeople: React.Dispatch<React.SetStateAction<Props["people"]>>
+    setPeople: React.Dispatch<React.SetStateAction<Props["people"]>>
+    people: Props["people"]
 }
 
-const AddToList: React.FC<IProps> = ( {people, setPeople}) => {
+const AddToList: React.FC<IProps> = ({setPeople, people}) => {
 
-   const [input, setInput] = useState({
-    name: "",
-    age: "",
-    note: "",
-    img: ""
-   })
+    const [input, setInput] = useState({
+        name: "",
+        age: "",
+        note: "",
+        img: ""
+    }) 
 
-   const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>): void => {
-      setInput({
-         ...input,
-         [e.target.name]: e.target.value
-      })
-   }
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+        setInput({
+            ...input,
+            [e.target.name]: e.target.value
+        })
+    }
 
-   const handleClick = ():void => {
-      if (
-         !input.name || 
-         !input.age ||
-         !input.img
-      ) {
-         return
-      }
+    const handleClick = () => {
+        if(!input.name || !input.age) return
 
-      setPeople([
-         ...people,
-         {
-            name: input.name,
-            age: parseInt(input.age),
-            url: input.img,
-            note: input.note
-         }
-      ])
+        setPeople([
+            ...people,
+            {
+                name: input.name,
+                age: parseInt(input.age),
+                img: input.img,
+                note: input.note
+            }
+        ]);
 
-   }
+        setInput({
+            name: "",
+            age: "",
+            img: "",
+            note: ""
+        })
+    }
 
-  return (
-    <div className='AddToList'>
-        <input 
-        type="text"
-        placeholder="Name"
-        className="AddToList-input"
-        value={input.name}
-        onChange={handleChange}
-        name="name"
-        />
-        <input 
-        type="text"
-        placeholder="Age"
-        className="AddToList-input"
-        value={input.age}
-        name="age"
-        />
-        <input 
-        type="text"
-        placeholder="Image Url"
-        className="AddToList-input"
-        value={input.img}
-        name="img"
-        />
-        <textarea
-        placeholder="Notes"
-        className="AddToList-input"
-        onChange={handleChange}
-        value={input.note}
-        name="notes"
-        />
-        <button className='AddToList-btn' onClick={handleClick}> 
-         Add to List
-        </button>
-    </div>
-  )
+    return (
+        <div className="AddToList">
+            <input 
+                type="text"
+                onChange={handleChange}
+                className="AddToList-input"
+                name="name"
+                value={input.name}
+                placeholder="Name"
+            />
+            <input 
+                type="text"
+                onChange={handleChange}
+                className="AddToList-input"
+                name="age"
+                value={input.age}
+                placeholder="Age"
+            />
+            <input 
+                type="text"
+                onChange={handleChange}
+                className="AddToList-input"
+                name="img"
+                value={input.img}
+                placeholder="Image Url"
+            />
+            <textarea
+                onChange={handleChange}
+                className="AddToList-input"
+                name="note"
+                value={input.note}
+                placeholder="Note"
+            />
+            <button
+                onClick={handleClick}
+                className="AddToList-btn"
+            >
+                Add to List
+            </button>
+        </div>
+    )
 }
 
-export default AddToList;
+export default AddToList
